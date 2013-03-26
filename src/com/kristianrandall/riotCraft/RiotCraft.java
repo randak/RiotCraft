dc9b6fc6b5481e02c7fa9dc397a468055ad868f5
Kristian Randall <kristian.l.randall@gmail.com>
2013-03-25 17:54:37 -0700

    Cleaned up code and added projectile launch listener

--- src/com/kristianrandall/riotCraft/RiotCraft.java
+++ src/com/kristianrandall/riotCraft/RiotCraft.java
@@ -13,15 +13,16 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.BlockIgniteEvent.IgniteCause;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.inventory.BrewEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.BrewerInventory;
@@ -59,16 +60,6 @@ public final class RiotCraft extends JavaPlugin implements Listener {
         if(event.getAction().equals(Action.RIGHT_CLICK_AIR) || (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) 
         		&& !this.getConfig().getIntegerList("ignoreThrow").contains(event.getClickedBlock().getTypeId()))){
             if(p.getItemInHand().getType() == Material.CLAY_BRICK && this.getConfig().getBoolean("brick.enabled")){
-            	
-//            	if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-//            		Material clickedBlock = event.getClickedBlock().getType();
-//            		
-////            		if()
-//            	}
-            	
-            	//TODO MAKE SURE NOT OPENING CHEST, etc (check clicked block)
-            	//if block is door, chest, workbench, furnace, potion stand, lever, button, repeater, item frame
-            	
             	if(!p.getGameMode().equals(GameMode.CREATIVE)) {
             		if(p.getItemInHand().getAmount() == 1) {
                 		p.getInventory().remove(p.getItemInHand());
@@ -112,13 +103,12 @@ public final class RiotCraft extends JavaPlugin implements Listener {
     			dropped.setPickupDelay(50000); //never pick it up
     			
     			FireChargeTimer hiss = new FireChargeTimer(dropped, "hiss");
-    			hiss.setID(this.getServer().getScheduler().scheduleSyncDelayedTask(this, hiss, 40L));
+    			this.getServer().getScheduler().scheduleSyncDelayedTask(this, hiss, 40L);
     			
     			FireChargeTimer explode = new FireChargeTimer(dropped, "explode");
-    			explode.setID(this.getServer().getScheduler().scheduleSyncDelayedTask(this, explode, 60L));
+    			this.getServer().getScheduler().scheduleSyncDelayedTask(this, explode, 60L);
             } else if(p.getItemInHand().getType() == Material.POTION && this.getConfig().getBoolean("molotov.enabled")) {
             	if(p.getItemInHand().getDurability() == 16384) {
-            		
             		int pots = (this.playerPotions.containsKey(p)) ? this.playerPotions.get(p)+1 : 1;
             		this.playerPotions.put(p, pots); //increment potions to burn
             	}
@@ -127,10 +117,35 @@ public final class RiotCraft extends JavaPlugin implements Listener {
     }
     
     /**
+     * Listens for projectile launch and throws molotovs at a configured velocity.
+     * 
+     * @param event
+     * @since 0.5
+     */
+    @EventHandler
+    public void onPotionThrow(ProjectileLaunchEvent event) {
+    	if(event.getEntity().getShooter() instanceof Player) {
+    		Player p = (Player) event.getEntity().getShooter();
+    		
+        	if(event.getEntityType().equals(EntityType.SPLASH_POTION) && this.playerPotions.containsKey(p) && this.playerPotions.get(p) > 0) {
+    	    	
+    	    	Projectile proj = event.getEntity();
+    	    	
+    	    	Location loc = p.getEyeLocation();
+    			Location b = p.getTargetBlock(null, 20).getLocation();
+    	    	
+    	    	Vector v = new Vector((b.getX() - loc.getX()) / 10D, (b.getY() - loc.getY()) / 10D, (b.getZ() - loc.getZ()) / 10D);
+    			
+    			proj.setVelocity(v.normalize().multiply(this.getConfig().getDouble("molotov.velocity")));
+        	}
+    	}
+    }
+    
+    /**
      * Listens for a BlockIgniteEvent and stops fire charges from creating fires.
      * 
      * @param event an event triggered on block ignition
-     * @since 1.5
+     * @since 0.5
      */
     @EventHandler
     public void onBlockIgnite(BlockIgniteEvent event) {
@@ -158,21 +173,16 @@ public final class RiotCraft extends JavaPlugin implements Listener {
 		}
 	}
     
-    public void potionBrewer() {
-    	//TODO set custom recipe for splash potion of fire and prevent default recipes
-    }
-    
     @EventHandler
     public void potionBrewListener(BrewEvent event) {
     	BrewerInventory inv = event.getContents();
     	
-    	List<HumanEntity> players = inv.getViewers();
-    	
     	//for each of the slots in the brew stand
     	for(ItemStack is : inv.getContents()) {
-    		((Player)players.get(0)).sendMessage("Durability ="+is.getDurability());
-    		if(is.getDurability() == 16384 || is.getDurability() == 8192) { //Set splash mundane potion to splash potion of fire
-	    		//new potionmeta object
+    		if(is != null && is.getType().equals(Material.POTION) && (is.getDurability() == 16384 || is.getDurability() == 8192)) { 
+    			//Set splash mundane potion to splash potion of fire
+	    		
+    			//new potionmeta object
     			PotionMeta m = (PotionMeta) is.getItemMeta();
     			
 				//Sets the potion name
